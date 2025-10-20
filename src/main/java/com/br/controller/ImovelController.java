3459c05c9b127be40550fc9e3452b0c1690c6be0
alissonhfp2003 <129643644+alissonhfp2003@users.noreply.github.com>
2025-10-19 23:18:04 -0300

    Adiciona operações CRUD para  e Imovel nos respectivos controladores

--- src/main/java/com/br/controller/ImovelController.java
+++ src/main/java/com/br/controller/ImovelController.java
@@ -5,8 +5,12 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,16 +23,12 @@ import com.br.repository.ImovelRepository;
 @CrossOrigin(origins="*")
 public class ImovelController {
 
-	//Cria o repositorio JPA para ser usado aqui no controlador
 	@Autowired
 	private ImovelRepository imovelrep;
 
 
-	@GetMapping("/imovel")  //Indica que esse será o nome do endereço a ser chamado
+	@GetMapping("/imovel")
 	public List<Imovel> listar(){
-
-		//para chamar o "listar", o endereço completo deverá ser:
-		// http://localhost:8080/cimovel/imovel -- usando o protocolo http, método GET
 
 		return imovelrep.findAll();
 
@@ -43,6 +43,39 @@ public class ImovelController {
 		return ResponseEntity.ok(imovel);
 	}
 
+	@PostMapping("/imovel")
+	public Imovel inserir(@RequestBody Imovel imovel) {
+		return imovelrep.save(imovel);
+	}
 
+	@PutMapping("/imovel/{id}")
+	public ResponseEntity<Imovel> alterar(@PathVariable Long id, @RequestBody Imovel imovelAtualizado) {
+
+		Imovel imovel = imovelrep.findById(id).orElseThrow(()->
+		new ResourceNotFoundException("Imovel nao encontrado."));
+
+		imovel.setEndereco(imovelAtualizado.getEndereco());
+		imovel.setTipo(imovelAtualizado.getTipo());
+		imovel.setDataConstrucao(imovelAtualizado.getDataConstrucao());
+		imovel.setPrecoVenda(imovelAtualizado.getPrecoVenda());
+		imovel.setMobiliado(imovelAtualizado.getMobiliado());
+		imovel.setQuantidade(imovelAtualizado.getQuantidade());
+
+		Imovel imovelSalvo = imovelrep.save(imovel);
+
+		return ResponseEntity.ok(imovelSalvo);
+	}
+
+	@DeleteMapping("/imovel/{id}")
+	public ResponseEntity<Void> excluir(@PathVariable Long id) {
+
+
+		Imovel imovel = imovelrep.findById(id).orElseThrow(()->
+		new ResourceNotFoundException("Imovel nao encontrado."));
+
+		imovelrep.delete(imovel);
+
+		return ResponseEntity.noContent().build();
+	}
 
 }
